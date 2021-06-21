edef777e452768dc13c8c133337f11b0ff002ea6
George Sheng <gsheng112@gmail.com>
2021-06-21 13:46:33 -0400

    added listener

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ console.log("Here");
 for(let i = 0; i < 10; i++){
     console.log(i);
 }
+document.addEventListener("onclick", (e) => {console.log("clicked")});
 
 //init();
 //setInterval(frame, 20);
