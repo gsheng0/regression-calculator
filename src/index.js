6cf41cd12366a7b771871e6de72047b9f0cac71b
George Sheng <gsheng112@gmail.com>
2021-06-21 13:34:48 -0400

    for loop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import {Draw} from "./draw.js";
+
 
 //var canvas, ctx, location, points, op;
 
 console.log("Here");
 
-
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
 
 //init();
 //setInterval(frame, 20);
