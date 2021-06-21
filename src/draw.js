3041093d9cddc4f62df691b5cd578c713c552e20
George Sheng <gsheng112@gmail.com>
2021-06-21 10:51:38 -0400

    added base files

---
+++ src/draw.js
@@ -0,0 +1,66 @@
+import {Point} from "./point";
+
+export class Draw {
+    static ctx = undefined;
+    static WHITE = "white";
+    static BLACK = "#000000";
+    static CANVAS = undefined;
+
+    static setCanvas(canvas){
+        Draw.CANVAS = canvas;
+        Draw.ctx = canvas.getContext("2d");
+    }
+
+    static fillRect(x, y, width, height, color){
+        let ctx = Draw.ctx;
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.rect(x, y, width, height);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    static strokeRect(x, y, width, height, color){
+        let ctx = Draw.ctx;
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.rect(x, y, width, height);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    static drawLine(x1, y1, x2, y2, color){
+        let ctx = Draw.ctx;
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    static fillCircle(x, y, radius, color){
+        let ctx = Draw.ctx;
+        ctx.beginPath();
+        ctx.fillStyle = color;
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    static drawFunction(func){
+        let previous = new Point(0, func[0], Point.GRAPH);
+        for(let x = 0; x < 1400; x++){
+            let out = 0;
+            for(let i = 0; i < func.length; i++){
+                out += func[i] * Math.pow(x, i);
+            }
+            let point = new Point(x, out, Point.GRAPH);
+            if(out < 0)
+                continue;
+            Draw.fillCircle(point.getScreenPoint().x, point.getScreenPoint().y, 0.25, Draw.BLACK);
+            Draw.drawLine(previous.getScreenPoint().x, previous.getScreenPoint().y, point.getScreenPoint().x, point.getScreenPoint().y);
+            previous = point;
+        }
+    }
+}
---
+++ src/point.js
@@ -0,0 +1,30 @@
+
+export class Point{
+    static SCREEN = 0;
+    static GRAPH = 1;
+    static CANVAS_HEIGHT = 0;
+    static CANVAS_WIDTH = 0;
+    constructor(x, y, mode = Point.SCREEN){
+        this.x = x;
+        this.y = y;
+        this.mode = mode;
+    }
+
+    getScreenPoint(){
+        if(this.mode === Point.SCREEN){
+            return new Point(this.x, this.y, Point.SCREEN);
+        }
+        return new Point(this.x + 30, -1 * this.y + (Point.CANVAS_HEIGHT - 30));
+    }
+
+    getGraphPoint(){
+        if(this.mode === Point.GRAPH){
+            return new Point(this.x, this.y, Point.GRAPH);
+        }
+        return new Point(this.x - 30, -1 * this.y + (Point.CANVAS_HEIGHT + 30) - 60, Point.GRAPH);
+    }
+
+    toString() { 
+        return this.x + ", " + this.y
+    }
+}
